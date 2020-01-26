33f014a63f36137e09c1d9f7be453aa6aab6bf13
Okubyona <andrewbazua@gmail.com>
2020-01-25 20:09:05 -0800

    Feat: implement sphere Intersect function
    sphere.cpp:Intersection()
        calculate quadratic equation to determine if given ray
        intersects with the sphere. If they do, function will return the
        solution that is closest.

--- sphere.cpp
+++ sphere.cpp
@@ -1,11 +1,51 @@
 #include "sphere.h"
 #include "ray.h"
 
+#include <cmath>
+
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {0,0,0};
+    // this fuction is essentially just solving the quadratic equation.
+    Hit intersection; //declare empty hit
+    intersection.object = NULL;
+    intersection.part = -1;
+
+    // values for a,b,c determined from equation in the textbook
+    double a;
+    double b;
+    double c;
+    double descriminant;
+    double t_1; // solution 1
+    double t_2; // soultion 2
+
+    a = ray.direction.magnitude_squared();
+    b = 2 * dot(ray.direction, (ray.endpoint - center));
+    c = (ray.endpoint - center).magnitude_squared() - (radius * radius);
+    descriminant = (b * b) - (4 * a * c);
+
+    if (descriminant < 0) {
+        // ray does not intersect sphere, return null hit
+        return intersection;
+    }
+
+    t_1 = (-b + sqrt(descriminant)) / (2 * a);
+    t_2 = (-b - sqrt(descriminant)) / (2 * a);
+
+    //now we determine which t is closest and higher than threshold small_t
+    if (t_1 < t_2 && t_1 >= small_t) {
+        intersection.dist = t_1;
+        intersection.object = this;
+        intersection.part = part;
+    }
+
+    else if (t_2 < t_1 && t_2 >= small_t) {
+        intersection.dist = t_1;
+        intersection.object = this;
+        intersection.part = part;
+    }
+
+    return intersection;
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
