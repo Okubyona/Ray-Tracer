76028475ee47b2da72662c955c054563ed985712
Okubyona <andrewbazua@gmail.com>
2020-01-27 21:16:52 -0800

    Feat: implement full phong shader
    Implement ambient, diffuse, and specular reflections according to the
        formulas given in the lecture slides and textbook.

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -4,11 +4,40 @@
 #include "render_world.h"
 #include "object.h"
 
+#include <cmath>
+
 vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-    TODO; //determine the color
-    return color;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    vec3 L;
+    vec3 R;
+
+    // Should first use world's ambient color and intensity first
+    // to determine the ambient reflection
+
+    ambient = color_ambient * world.ambient_color * world.ambient_intensity;
+
+    //iterate through the number of lights in the world
+    for (unsigned i = 0; i < world.lights.size(); ++i) {
+        L = world.lights[i]->position - intersection_point;
+        //equation for R is taken from the textbook
+        R = (-L + 2 * dot(L, normal) * normal).normalized();
+
+        diffuse += color_diffuse * world.lights[i]->Emitted_Light(L)
+                    * std::max(dot(normal, L.normalized()), 0.0);
+
+        // Not neccessary for Checkpoint 1, but L and R are computed and formula
+        // is given in the lecture slides
+        specular += color_specular * world.lights[i]->Emitted_Light(L)
+                    * std::pow(std::max(dot(R, -(ray.direction)), 0.0), specular_power);
+
+    }
+
+    return ambient + diffuse + specular;
 }
